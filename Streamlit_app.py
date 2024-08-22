d0023a195eac13e91654911c0271ffa7a56dce08
Vignesh08-29 <vikiabirohith@gmail.com>
2024-08-22 14:24:03 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -39,4 +39,4 @@ if ingredients_Lists:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
